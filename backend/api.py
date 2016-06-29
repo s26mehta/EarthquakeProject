0d50c5c1cee2c53a0574fd2af34c8242ab9ce890
Matthew Gougeon <mtthw.gougeon@gmail.com>
2016-06-29 14:34:46 -0400

    preliminary functions seem to work

--- backend/api.py
+++ backend/api.py
@@ -7,24 +7,33 @@ app = Bottle()
 logging.basicConfig(filename='log.log', format='%(asctime)s-%(name)s\t-%(levelname)s\t-%(message)s',level=logging.DEBUG)
 log = logging.getLogger('log')
 
-isEarthquake = False
+class Location():
+    """geo location"""
+    def __init__(self, _lat = None, _lon = None):
+        super(location, self).__init__()
+        self.lat = _lat
+        self.lon = _lon
 
 class Person():
     """docstring for Person"""
-    def __init__(self, _name, _status):
+    def __init__(self, _name, _status, _location = None):
         self.name = _name
         self.status = _status
+        self.location = _location
 
     def isSafe(self):
         if self.status == 1: # 1 is safe
             return True
         return False
 
+    def updateLocation(self, _loc):
+        self.location = _loc
 
 class People():
     """Object for all people in the database"""
     def __init__(self, _array = []):
         self.array = _array
+        self.isEarthquake = False
 
     def newPerson(self, _name, _status):
         self.array.append(Person(_name, _status))
@@ -35,9 +44,18 @@ class People():
             if i.isSafe():
                 isSafe.append(i)
         return isSafe
+
+    def setSafe(self, _name):
+        for i in self.array:
+            if i.name == _name:
+                i.status = 1
+
+    def setNewLocation(self, _name, _loc):
+        for i in self.array:
+            if i.name == _name:
+                i.location = _loc
         
 people = People()
-
 
 @app.route('/')
 def home():
@@ -45,20 +63,36 @@ def home():
 
 @app.get('/isEarthquake')
 def get_earthquake_now():
-    return isEarthquake
+    log.debug("isearthquake: " + str(people.isEarthquake))
+    return str(people.isEarthquake)
 
 @app.post('/setEarthquake')
 def set_earthquake_now():
     forms = request.forms
-    if forms['eq'] == 1:
-        isEarthquake = True
+    if forms['eq'] == str(1):
+        people.isEarthquake = True
     else:
-        isEarthquake = False
+        people.isEarthquake = False
+    log.debug(people.isEarthquake)
 
 @app.get('/getSafe')
 def getSafePeople():
     return people.whoIsSafe()
 
+@app.post('/newPerson')
+def newPerson():
+    return people.new(request.forms['name'])
+
+@app.post('/setSafe')
+def setSafePerson(self):
+    forms = request.forms
+    people.setSafe(forms['name'])
+
+    
+@app.post('/newLocation')
+def updateLocation(self):
+    forms = request.forms
+    people.setNewLocation(forms['name'], Location(forms['lat'], forms['lon']))
 
 
 class StripPathMiddleware(object):
