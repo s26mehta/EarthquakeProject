fa54206dbafd3f91bb3840286f32beee1906ce7a
Matthew Gougeon <mtthw.gougeon@gmail.com>
2016-07-04 15:47:57 -0400

    basic heatmap funtionality works

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,5 @@
 from bottle import *
+from classes import *
 import logging
 import json
 
@@ -8,76 +9,19 @@ app = Bottle()
 logging.basicConfig(filename='log.log', format='%(asctime)s-%(name)s\t-%(levelname)s\t-%(message)s',level=logging.DEBUG)
 log = logging.getLogger('log')
 
-class Location():
-    """geo location"""
-    def __init__(self, _lat = None, _lon = None):
-        self.lat = _lat
-        self.lon = _lon
-
-    def dump(self):
-        return '{"lat": ' + str(self.lat) + ', "lon": ' + str(self.lon) +'}'
-
-class Person():
-    """docstring for Person"""
-    def __init__(self, _name, _status = 0, _location = None):
-        self.name = _name
-        self.status = _status
-        self.location = _location
-
-    def dump(self):
-        if self.location:
-            return '{"name":'+str(self.name)+', "status": '+str(self.status)+', "location": '+str(self.location.dump())+'}'
-        return '{"name":'+str(self.name)+', "status": '+str(self.status)+'}'
-
-
-    def isSafe(self):
-        if self.status == 1: # 1 is safe
-            return True
-        return False
-
-    def updateLocation(self, _loc):
-        self.location = _loc
-
-class People():
-    """Object for all people in the database"""
-    def __init__(self, _array = []):
-        self.array = _array
-        self.isEarthquake = False
-
-    def newPerson(self, _name):
-        self.array.append(Person(_name))
-
-    def whoIsSafe(self):
-        isSafe = []
-        for i in self.array:
-            if i.isSafe():
-                isSafe.append(i)
-        ret = ""
-        for i in isSafe:
-            ret += i.dump()
-        return ret
-
-    def everyone(self):
-        ret = "["
-        for i in self.array:
-            ret += i.dump() +","
-        return ret[:-1] +"]"
- 
-    def setSafe(self, _name):
-        for i in self.array:
-            if i.name == _name:
-                i.status = 1
-
-    def setNewLocation(self, _name, _loc):
-        for i in self.array:
-            if i.name == _name:
-                i.location = _loc
-        
 people = People()
+
+#######################################################
+###                API Routes                       ###
+#######################################################
 
 @app.route('/')
 def home():
     return static_file("index.html", root='.')
+
+@app.route('/map')
+def map():
+    return static_file("map.html", root='html')
 
 @app.get('/isEarthquake')
 def get_earthquake_now():
@@ -110,12 +54,15 @@ def setSafePerson():
     forms = request.forms
     people.setSafe(forms['name'])
 
+@app.post('/setUnSafe')
+def setSafePerson():
+    forms = request.forms
+    people.setUnSafe(forms['name'])
     
 @app.post('/newLocation')
 def updateLocation():
     forms = request.forms
     people.setNewLocation(forms['name'], Location(forms['lat'], forms['lon']))
-
 
 class StripPathMiddleware(object):
     '''
---
+++ backend/classes.py
@@ -0,0 +1,71 @@
+class Location():
+    """geo location"""
+    def __init__(self, _lat = None, _lon = None):
+        self.lat = _lat
+        self.lon = _lon
+
+    def dump(self):
+        return '{"lat": ' + str(self.lat) + ', "lon": ' + str(self.lon) +'}'
+
+class Person():
+    """docstring for Person"""
+    def __init__(self, _name, _status = "unsafe", _location = None):
+        self.name = _name
+        self.status = _status
+        self.location = _location
+
+    def dump(self):
+        if self.location:
+            return '{"name": "'+str(self.name)+'", "status": "'+str(self.status)+'", "location": '+str(self.location.dump())+'}'
+        return '{"name":"'+str(self.name)+'", "status": "'+str(self.status)+'""}'
+
+
+    def isSafe(self):
+        if self.status == "safe": # 1 is safe
+            return True
+        return False
+
+    def updateLocation(self, _loc):
+        self.location = _loc
+
+class People():
+    """Object for all people in the database"""
+    def __init__(self, _array = []):
+        self.array = _array
+        self.isEarthquake = False
+
+    def newPerson(self, _name):
+        self.array.append(Person(_name))
+
+    def whoIsSafe(self):
+        isSafe = []
+        for i in self.array:
+            if i.isSafe():
+                isSafe.append(i)
+        ret = ""
+        for i in isSafe:
+            ret += i.dump()
+        return ret
+
+    def everyone(self):
+        if self.array:
+            ret = "["
+            for i in self.array:
+                ret += i.dump() +","
+            return ret[:-1] +"]"
+        return '[]'
+ 
+    def setSafe(self, _name):
+        for i in self.array:
+            if i.name == _name:
+                i.status = "safe"
+
+    def setUnSafe(self, _name):
+        for i in self.array:
+            if i.name == _name:
+                i.status = "unsafe"
+
+    def setNewLocation(self, _name, _loc):
+        for i in self.array:
+            if i.name == _name:
+                i.location = _loc
